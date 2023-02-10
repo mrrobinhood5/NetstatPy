52487f487222746605d6c566f27d84816767ba00
Rob M <mrrobinhood5@gmail.com>
2023-02-10 09:29:55 -0600

    removed unnecesarry things

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from time import sleep
 # define variables
 DB_USER = os.environ['DB_USER']
 DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_URL = os.environ['DB_URL']
-loop_count = 0
+# DB_URL = os.environ['DB_URL']
+# loop_count = 0
 device_connected = False
-CST = pytz.timezone('US/Central')
-dt = datetime.datetime.now(tz=CST).ctime()
+# CST = pytz.timezone('US/Central')
+# dt = datetime.datetime.now(tz=CST).ctime()
 BLACKLIST_IPS = ['', ['31', '13'], ['157', '240'], ['74', '201'], ['127', '0']]
 client = pymongo.MongoClient(
     f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.i31qn.mongodb.net/?retryWrites=true&w=majority")
@@ -18,6 +18,7 @@ db = client.Pinger.data
 
 def is_blacklisted(ip_address):
     return ip_address.split('.')[:2] in BLACKLIST_IPS
+
 
 def main():
     # display version number
@@ -31,7 +32,7 @@ def main():
     # check adb devices to see if the headset is connected
     print(f'\n---\nOkay, connect the Quest wired and click Allow when it asks you.\n\n'
           f'Also also, make sure this computer is on the SAME network as the quest. \n\n')
-
+    sleep(10)
 
     # print("Disconnecting previous sessions...")
     # p = subprocess.Popen('adb disconnect', shell=True)
@@ -55,10 +56,11 @@ def main():
     while True:
         whois = []
         pings = []
+        # loop_count = 0
 
         # grab raw output from NETSTAT and cleanup raw
         print(f'Grabbing Connections...')
-        connected_ips = subprocess.Popen(f"adb -s {quest_ip}:5555 shell netstat -tn | grep ESTABLISHED",
+        connected_ips = subprocess.Popen(f"adb -s {ip_address}:5555 shell netstat -tn | grep ESTABLISHED",
                                          shell=True, stdout=subprocess.PIPE).stdout.readlines()
         # split all lines into columns, remove ipv6S and remove duplicates
         connected_ips = set([x[4].split(b':')[0].decode('utf-8') for x in
@@ -81,7 +83,8 @@ def main():
         # ping all the IPs
         for ip in connected_ips:
             print(f'pinging ip {ip}..\n')
-            p = subprocess.Popen(f"adb -s {quest_ip}:5555 shell ping -c 10 {ip}", shell=True, stdout=subprocess.PIPE).stdout.readlines()[-1]
+            p = subprocess.Popen(f"adb -s {ip_address}:5555 shell ping -c 10 {ip}", shell=True,
+                                 stdout=subprocess.PIPE).stdout.readlines()[-1]
             if p == b'\n':
                 pings.append(0)
             else:
@@ -91,7 +94,7 @@ def main():
         db_data = []
         z = list(zip(connected_ips, whois, pings))
         for x in z:
-            db_data.append({'tester_name': tester_name, 'location_info': location_info, 'loop_count': loop_count,
+            db_data.append({'tester_name': tester_name, 'location_info': location_info,
                             'ip': x[0], 'name': x[1], 'ping': float(x[2])})
 
         # write to the db
@@ -100,10 +103,11 @@ def main():
             print(f'Dumped loop {db_data} into db')
             print(f'Waiting 5 minutes to re-ping\n\n')
             sleep(300)
-            loop_count += 1
+            # loop_count += 1
         else:
             print(f'No data... Waiting 30 seconds')
             sleep(30)
+
 
 if __name__ == '__main__':
     main()
